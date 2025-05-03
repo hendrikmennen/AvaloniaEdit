bbd880ed24bd5b30de06ab44b03939f08a21aa0b
Hendrik Mennen <hmennen@protop-solutions.com>
2025-05-03 16:52:46 +0200

    .

--- src/AvaloniaEdit/Document/DataObjectCopyingEventArgs.cs
+++ src/AvaloniaEdit/Document/DataObjectCopyingEventArgs.cs
@@ -4,11 +4,17 @@ using AvaloniaEdit.Utils;
 
 namespace AvaloniaEdit.Document;
 
-public class DataObjectCopyingEventArgs(IDataObject dataObject, bool isDragDrop) :
-    RoutedEventArgs(DataObjectEx.DataObjectCopyingEvent)
+public class DataObjectCopyingEventArgs :
+    RoutedEventArgs
 {
+    public DataObjectCopyingEventArgs(IDataObject dataObject, bool isDragDrop) : base(DataObjectEx.DataObjectCopyingEvent)
+    {
+        DataObject = dataObject;
+        IsDragDrop = isDragDrop;
+    }
+
     public bool CommandCancelled { get; private set; }
-    public IDataObject DataObject { get; } = dataObject;
-    public bool IsDragDrop { get; } = isDragDrop;
+    public IDataObject DataObject { get; }
+    public bool IsDragDrop { get; }
     public void CancelCommand() => CommandCancelled = true;
 }
